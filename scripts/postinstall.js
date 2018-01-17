2078ee6c49d07ca67e1a4e8e3588b35d2be545e4
Claudio Rodriguez <claudio.rdgz@gmail.com>
2018-01-17 12:44:23 -0500

    bug: installing configs on wrong folder

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,7 +5,7 @@ function copyConfig(configName) {
   const pathToConfig = path.join(__dirname, '../', configName)
 
   const pathToNewConfigDir = path.dirname(process.cwd())
-  const pathToNewConfig = path.join(pathToNewConfigDir, configName)
+  const pathToNewConfig = path.join(pathToNewConfigDir, '../', configName)
 
   console.info(`Copying ${pathToConfig} to => ${pathToNewConfig}`)
   fs.createReadStream(pathToConfig).pipe(fs.createWriteStream(pathToNewConfig))
